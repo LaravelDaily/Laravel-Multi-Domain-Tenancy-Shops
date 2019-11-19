52be70915fad7e7054b394d0e466f70793ec451d
Marius-K <marre124@gmail.com>
2019-11-19 14:26:16 +0200

    Created table seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,6 +14,15 @@ class UsersTableSeeder extends Seeder
                 'email'          => 'admin@admin.com',
                 'password'       => '$2y$10$bvYhN6JwpRJ/3e8mwEzpiu9YUxDi0QfbJM3GjQAa9fiz0QE5SACB6',
                 'remember_token' => null,
+                'subdomain'      => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'Company',
+                'email'          => 'company@company.com',
+                'password'       => '$2y$10$bvYhN6JwpRJ/3e8mwEzpiu9YUxDi0QfbJM3GjQAa9fiz0QE5SACB6',
+                'remember_token' => null,
+                'subdomain'      => 'company',
             ],
         ];
 
