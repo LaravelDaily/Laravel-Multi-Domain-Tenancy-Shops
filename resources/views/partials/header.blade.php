baa0d5028d15a550a2d9fd699fa64e50026a68c9
Marius-K <marre124@gmail.com>
2019-11-20 10:44:10 +0200

    Front page menu items

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,31 @@
             <div class="col-md-12">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <a class="navbar-brand" href="{{ url('/') }}">Listing</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-menu"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            @auth
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.products.index') }}">My Products</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                                </li>
+                                <form id="logout-form" class="d-none" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                </form>
+                            @else
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
+                                </li>
+                            @endauth
+                        </ul>
+                    </div>
                 </nav>
             </div>
         </div>
